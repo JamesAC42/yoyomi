cef26693ea4606dbcc8d79d179561d0db5dc461f
James Crovo <jamescrovo2019@lschs.org>
2019-08-04 02:32:41 -0400

    Add back button to error screen

--- client/src/js/components/Loading.js
+++ client/src/js/components/Loading.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import "../../css/Loading.css";
 
 export class Error extends Component {
     render(){
         return(
             <div className="content">
+                <div className="thread-header">
+                    <div className="back-btn">
+                        <Link to={"/yoyomi/board/" + this.props.board}>
+                            &#60;&#60;Back
+                        </Link>
+                    </div>
+                </div>
                 <div className="error-outer">
                     <div className="error-inner">
                         Something went wrong.
